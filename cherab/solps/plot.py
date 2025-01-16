1f11e1e2c341adc7fdabf5f0d887e7bd505b9794
svoboda <svoboda@ipp.cas.cz>
2025-01-16 14:11:11 +0100

    Reorganize functions so that high level one are at the top

--- cherab/solps/plot.py
+++ cherab/solps/plot.py
@@ -28,45 +28,6 @@ def plot_quadrangle_mesh(mesh, solps_data=None, ax=None):
     return ax
 
 
-def create_quadrangle_polycollection(mesh, solps_data=None, **collection_kw):
-    """
-    Creates a matplotlib PolyCollection object from the quadrangle mesh.
-
-    If solps_data is provided, it is used to colour the faces of the quadrangles in the mesh.
-
-    :param mesh: SOLPSMesh object
-    :param solps_data: Optional[np.ndarray] - Data array defined on the SOLPS mesh
-    :param collection_kw: Keyword arguments for the PolyCollection
-    
-    :return: matplotlib.collections.PolyCollection
-    """
-    verts = mesh.vertex_coordinates[mesh.quadrangles]
-    collection_mesh = PolyCollection(verts, **collection_kw)
-    if solps_data is not None:
-        collection_mesh.set_array(solps_data[mesh.quadrangle_to_grid_map[:, 0], mesh.quadrangle_to_grid_map[:, 1]])
-    return collection_mesh
-
-
-def _format_matplotlib_axes(ax, mesh=None):
-    """
-    Formats the matplotlib axes for a SOLPS mesh plot.
-
-    Sets aspect and labels for the axes. 
-    If a SOLPSMesh object is provided, sets the limits of the axes to the mesh extent.
-
-    :param ax: matplotlib axes
-    :param mesh: SOLPSMesh object (optional)
-    :return: matplotlib axes
-    """
-    ax.set_aspect(1)
-    ax.set_xlabel("R [m]")
-    ax.set_ylabel("z [m]")
-    if mesh is not None:
-        ax.set_xlim(mesh.mesh_extent["minr"], mesh.mesh_extent["maxr"])
-        ax.set_ylim(mesh.mesh_extent["minz"], mesh.mesh_extent["maxz"])
-    return ax
-
-
 def plot_triangle_mesh(mesh, solps_data=None, ax=None):
     """
     Plots the triangle mesh grid geometry to a matplotlib figure.
@@ -93,6 +54,25 @@ def plot_triangle_mesh(mesh, solps_data=None, ax=None):
     return ax
 
 
+def create_quadrangle_polycollection(mesh, solps_data=None, **collection_kw):
+    """
+    Creates a matplotlib PolyCollection object from the quadrangle mesh.
+
+    If solps_data is provided, it is used to colour the faces of the quadrangles in the mesh.
+
+    :param mesh: SOLPSMesh object
+    :param solps_data: Optional[np.ndarray] - Data array defined on the SOLPS mesh
+    :param collection_kw: Keyword arguments for the PolyCollection
+    
+    :return: matplotlib.collections.PolyCollection
+    """
+    verts = mesh.vertex_coordinates[mesh.quadrangles]
+    collection_mesh = PolyCollection(verts, **collection_kw)
+    if solps_data is not None:
+        collection_mesh.set_array(solps_data[mesh.quadrangle_to_grid_map[:, 0], mesh.quadrangle_to_grid_map[:, 1]])
+    return collection_mesh
+
+
 def create_triangle_polycollection(mesh, solps_data=None, **collection_kw):
     """
     Creates a matplotlib PolyCollection object from the triangle mesh.
@@ -108,3 +88,23 @@ def create_triangle_polycollection(mesh, solps_data=None, **collection_kw):
     if solps_data is not None:
         collection_mesh.set_array(solps_data[mesh.triangle_to_grid_map[:, 0], mesh.triangle_to_grid_map[:, 1]])
     return collection_mesh
+
+
+def _format_matplotlib_axes(ax, mesh=None):
+    """
+    Formats the matplotlib axes for a SOLPS mesh plot.
+
+    Sets aspect and labels for the axes. 
+    If a SOLPSMesh object is provided, sets the limits of the axes to the mesh extent.
+
+    :param ax: matplotlib axes
+    :param mesh: SOLPSMesh object (optional)
+    :return: matplotlib axes
+    """
+    ax.set_aspect(1)
+    ax.set_xlabel("R [m]")
+    ax.set_ylabel("z [m]")
+    if mesh is not None:
+        ax.set_xlim(mesh.mesh_extent["minr"], mesh.mesh_extent["maxr"])
+        ax.set_ylim(mesh.mesh_extent["minz"], mesh.mesh_extent["maxz"])
+    return ax
